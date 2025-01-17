b1a2d8a2fa2eb7e1ed91332f8873587380b98655
Alvaro Parada <alvaro.parada92@gmail.com>
2025-01-17 19:53:34 +0100

    Add reporter to vite test config

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
       provider: "v8",
       include: ["src/**/*.{js,jsx,ts,tsx}"],
       exclude: ["src/main.jsx"],
+      reporter: ["lcov", "text"],
     },
   },
 });
