fe0f5d703b9579e46012f7b715c1cea0105b74fc
Alvaro Parada <alvaro.parada92@gmail.com>
2025-01-18 10:04:03 +0100

    Implement PhoneContext in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App/App.jsx";
+import { PhoneProvider } from "./contexts/PhoneContext/PhoneContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <PhoneProvider>
+      <App />
+    </PhoneProvider>
   </StrictMode>
 );
