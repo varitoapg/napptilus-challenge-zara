911df0c171cae06577cfd636e4cbdb365a34d2b3
Alvaro Parada <alvaro.parada92@gmail.com>
2025-01-18 12:36:58 +0100

    Test PhoneCard and create a more complex phone mock

--- src/mocks/phones/phones.js
+++ src/mocks/phones/phones.js
@@ -2,3 +2,10 @@ export const mockPhones = [
   { name: "Phone 1", id: 1 },
   { name: "Phone 2", id: 2 },
 ];
+
+export const mockPhoneOfList = {
+  imageUrl: "https://example.com/phone.jpg",
+  name: "Phone Model",
+  brand: "Phone Brand",
+  basePrice: 999,
+};
