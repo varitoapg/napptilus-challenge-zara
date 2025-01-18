db4039f7ef6a676cf120df0ddb92db7426d827b7
Alvaro Parada <alvaro.parada92@gmail.com>
2025-01-18 11:21:27 +0100

    Test App with routes

--- src/components/App/App.test.jsx
+++ src/components/App/App.test.jsx
@@ -1,13 +1,42 @@
+import { render, screen, cleanup } from "@testing-library/react";
+import { describe, it, expect, afterEach } from "vitest";
+import { MemoryRouter } from "react-router-dom";
 import App from "./App.jsx";
-import { describe, it, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
 
 describe("App", () => {
-  it("renders the heading", () => {
-    render(<App />);
+  afterEach(cleanup);
+
+  it("renders the heading and Phones page", () => {
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <App />
+      </MemoryRouter>
+    );
 
     const heading = screen.getByRole("heading", { name: "Zara Challenge" });
+    const secondHeading = screen.getByRole("heading", {
+      name: "List of phones",
+      level: 2,
+    });
 
-    expect(heading).toBeDefined();
+    expect(heading).not.toBeNull();
+    expect(secondHeading).not.toBeNull();
+  });
+
+  it("renders the heading and Phone detail when navigate to /phone/123", () => {
+    render(
+      <MemoryRouter initialEntries={["/phone/123"]}>
+        <App />
+      </MemoryRouter>
+    );
+
+    const heading = screen.getByRole("heading", { name: "Zara Challenge" });
+    const secondHeading = screen.getByRole("heading", {
+      name: "Phone detail",
+      level: 2,
+    });
+
+    expect(heading).not.toBeNull();
+    expect(secondHeading).not.toBeNull();
   });
 });
