40d9ad5db0c33ad6de49fea4c385dcd5aca86b38
Alvaro Parada <alvaro.parada92@gmail.com>
2025-01-18 11:20:38 +0100

    Implement routes in main and App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,17 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import PhonesPage from "../../pages/PhonesPage/PhonesPage";
+import PhoneDetailPage from "../../pages/PhoneDetailPage/PhoneDetailPage";
 import "./App.css";
 
 function App() {
   return (
     <>
       <h1>Zara Challenge</h1>
+      <Routes>
+        <Route path="/" element={<PhonesPage />} />
+        <Route path="/phone/:phoneId" element={<PhoneDetailPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./components/App/App.jsx";
 import { PhoneProvider } from "./contexts/PhoneContext/PhoneContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <PhoneProvider>
-      <App />
-    </PhoneProvider>
+    <BrowserRouter>
+      <PhoneProvider>
+        <App />
+      </PhoneProvider>
+    </BrowserRouter>
   </StrictMode>
 );
