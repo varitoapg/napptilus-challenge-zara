b4945e04f7d89f0b8f6de3d9b467ea7120b3c2fb
Alvaro Parada <alvaro.parada92@gmail.com>
2025-01-18 15:02:36 +0100

    Update usePhones to return totalPhones too

--- src/hooks/usePhones/usePhones.js
+++ src/hooks/usePhones/usePhones.js
@@ -4,9 +4,11 @@ import { usePhoneContext } from "../../contexts/PhoneContext/PhoneContext";
 export const usePhones = (search = "") => {
   const { phones, loading, error, loadPhones } = usePhoneContext();
 
+  const totalPhones = phones.length;
+
   useEffect(() => {
     loadPhones(search);
   }, [loadPhones, search]);
 
-  return { phones, loading, error };
+  return { phones, loading, error, totalPhones };
 };
