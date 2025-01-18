11cfd480050c8b5c83a061923cb89344511b2d7e
Alvaro Parada <alvaro.parada92@gmail.com>
2025-01-18 13:18:44 +0100

    Test PhoneCardList and add a mock of list of phones

---
+++ src/components/PhoneCardList/PhoneCardList.test.jsx
@@ -0,0 +1,32 @@
+import { render, cleanup, screen } from "@testing-library/react";
+import { describe, it, expect, afterEach } from "vitest";
+import PhoneCardList from "./PhoneCardList";
+import { mockListOfPhones } from "../../mocks/phones/phones";
+
+describe("PhoneCardList", () => {
+  afterEach(cleanup);
+
+  const phones = mockListOfPhones;
+
+  it("renders without crashing", () => {
+    render(<PhoneCardList phones={phones} />);
+  });
+
+  it("renders the correct number of PhoneCard components", () => {
+    render(<PhoneCardList phones={phones} />);
+    const phoneCards = screen.queryAllByTestId("phone-card");
+
+    expect(phoneCards.length).toBe(phones.length);
+  });
+
+  it("passes the correct props to each PhoneCard component", () => {
+    render(<PhoneCardList phones={phones} />);
+    const phoneCards = screen.queryAllByTestId("phone-card");
+
+    Array.from(phoneCards).forEach((phoneCard, index) => {
+      expect(phoneCard).not.toBeNull(phones[index].name);
+      expect(phoneCard).not.toBeNull(phones[index].brand);
+      expect(phoneCard).not.toBeNull(`${phones[index].basePrice} EUR`);
+    });
+  });
+});
--- src/mocks/phones/phones.js
+++ src/mocks/phones/phones.js
@@ -9,3 +9,20 @@ export const mockPhoneOfList = {
   brand: "Phone Brand",
   basePrice: 999,
 };
+
+export const mockListOfPhones = [
+  {
+    id: 1,
+    name: "Phone 1",
+    brand: "Brand A",
+    basePrice: 699,
+    imageUrl: "/images/phone1.jpg",
+  },
+  {
+    id: 2,
+    name: "Phone 2",
+    brand: "Brand B",
+    basePrice: 799,
+    imageUrl: "/images/phone2.jpg",
+  },
+];
