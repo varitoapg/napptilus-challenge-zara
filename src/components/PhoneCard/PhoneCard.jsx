56c817efec46dd9af2823014388fb8b49e9b6689
Alvaro Parada <alvaro.parada92@gmail.com>
2025-01-19 13:04:22 +0100

    Add Link to all PhoneCards and remove styles from anchors

--- src/components/PhoneCard/PhoneCard.jsx
+++ src/components/PhoneCard/PhoneCard.jsx
@@ -1,27 +1,31 @@
+import { Link } from "react-router-dom";
 import "./PhoneCard.css";
 import PropTypes from "prop-types";
 
 function PhoneCard({ phone }) {
   return (
-    <article className="phone-card" data-testid="phone-card">
-      <img
-        src={phone.imageUrl}
-        alt={phone.name}
-        className="phone-card__image"
-      />
-      <div className="phone-card__content">
-        <div className="phone-card__information">
-          <p className="phone-card__brand">{phone.brand}</p>
-          <p className="phone-card__name">{phone.name}</p>
+    <Link to={`/phone/${phone.id}`} className="phone-card__link">
+      <article className="phone-card" data-testid="phone-card">
+        <img
+          src={phone.imageUrl}
+          alt={phone.name}
+          className="phone-card__image"
+        />
+        <div className="phone-card__content">
+          <div className="phone-card__information">
+            <p className="phone-card__brand">{phone.brand}</p>
+            <p className="phone-card__name">{phone.name}</p>
+          </div>
+          <div className="phone-card__price">{phone.basePrice} EUR</div>
         </div>
-        <div className="phone-card__price">{phone.basePrice} EUR</div>
-      </div>
-    </article>
+      </article>
+    </Link>
   );
 }
 
 PhoneCard.propTypes = {
   phone: PropTypes.shape({
+    id: PropTypes.string.isRequired,
     imageUrl: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     brand: PropTypes.string.isRequired,
