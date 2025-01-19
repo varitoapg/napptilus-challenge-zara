cbded7b90cda219418be8de5c1a0eb27b2433aeb
Alvaro Parada <alvaro.parada92@gmail.com>
2025-01-19 11:27:49 +0100

    Add mocks for the new service

--- src/mocks/phones/phones.js
+++ src/mocks/phones/phones.js
@@ -26,3 +26,44 @@ export const mockListOfPhones = [
     imageUrl: "/images/phone2.jpg",
   },
 ];
+
+export const mockPhoneDetail = {
+  id: "Test main id",
+  brand: "Test brande",
+  name: "Test name",
+  description: "Test description",
+  basePrice: 619,
+  rating: 4.6,
+  specs: {
+    screen: "Test screen",
+    resolution: "Test resolution",
+    processor: "Test processor",
+    mainCamera: "Test camera",
+    selfieCamera: "Test MP",
+    battery: "No especificada",
+    os: "test",
+    storage: "128 GB",
+  },
+  colorOptions: [
+    {
+      name: "Color",
+      hexCode: "#1C1C1E",
+      imageUrl: "ColorImageUrl",
+    },
+  ],
+  storageOptions: [
+    {
+      capacity: "128 GB",
+      price: 619,
+    },
+  ],
+  similarProducts: [
+    {
+      id: "similarId",
+      brand: "Sim",
+      name: "Similar",
+      basePrice: 159,
+      imageUrl: "simularImageUrl",
+    },
+  ],
+};
--- src/mocks/phones/urls.js
+++ src/mocks/phones/urls.js
@@ -3,3 +3,5 @@ const API_URL = import.meta.env.VITE_APP_API_URL;
 export const basicMockedUrl = `${API_URL}/?limit=20&offset=0`;
 export const searchMockedUrl = `${API_URL}/?search=iphone&limit=20&offset=0`;
 export const limitOffsetMockedUrl = `${API_URL}/?limit=10&offset=20`;
+
+export const basicMockedDetailUrl = `${API_URL}/123`;
