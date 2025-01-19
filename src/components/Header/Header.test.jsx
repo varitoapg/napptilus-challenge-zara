3c0320637368fde434e692317449fd397118e2c1
Alvaro Parada <alvaro.parada92@gmail.com>
2025-01-19 11:06:16 +0100

    Test Header

---
+++ src/components/Header/Header.test.jsx
@@ -0,0 +1,78 @@
+import { cleanup, render, screen } from "@testing-library/react";
+import { vi, describe, it, expect, beforeEach } from "vitest";
+import Header from "./Header";
+import { useCartInformation } from "../../hooks/useCartInformation/useCartInformation";
+
+vi.mock("../../hooks/useCartInformation/useCartInformation");
+
+describe("Header", () => {
+  beforeEach(cleanup);
+
+  it("should render the logo", () => {
+    useCartInformation.mockReturnValue({
+      isCartVisible: false,
+      havePurchases: false,
+      phonesInCart: 0,
+    });
+
+    render(<Header />);
+
+    const logo = screen.getByTestId("logo");
+
+    expect(logo).not.toBeNull();
+  });
+
+  it("should not render the cart when isCartVisible is false", () => {
+    useCartInformation.mockReturnValue({
+      isCartVisible: false,
+      havePurchases: false,
+      phonesInCart: 0,
+    });
+
+    render(<Header />);
+
+    const phonesInCart = screen.queryByText("0");
+    const emptyCartIcon = screen.queryByTestId("empty-cart-icon");
+    const fullCartIcon = screen.queryByTestId("full-cart-icon");
+
+    expect(phonesInCart).toBeNull();
+    expect(emptyCartIcon).toBeNull();
+    expect(fullCartIcon).toBeNull();
+  });
+
+  it("should render the empty cart icon when there are no purchases", () => {
+    useCartInformation.mockReturnValue({
+      isCartVisible: true,
+      havePurchases: false,
+      phonesInCart: 0,
+    });
+
+    render(<Header />);
+
+    const phonesInCart = screen.queryByText("0");
+    const emptyCartIcon = screen.queryByTestId("empty-cart-icon");
+    const fullCartIcon = screen.queryByTestId("full-cart-icon");
+
+    expect(phonesInCart).not.toBeNull();
+    expect(emptyCartIcon).not.toBeNull();
+    expect(fullCartIcon).toBeNull();
+  });
+
+  it("should render the full cart icon when there are purchases", () => {
+    useCartInformation.mockReturnValue({
+      isCartVisible: true,
+      havePurchases: true,
+      phonesInCart: 3,
+    });
+
+    render(<Header />);
+
+    const phonesInCart = screen.queryByText("3");
+    const emptyCartIcon = screen.queryByTestId("empty-cart-icon");
+    const fullCartIcon = screen.queryByTestId("full-cart-icon");
+
+    expect(phonesInCart).not.toBeNull();
+    expect(emptyCartIcon).toBeNull();
+    expect(fullCartIcon).not.toBeNull();
+  });
+});
