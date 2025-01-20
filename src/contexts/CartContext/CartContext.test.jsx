8589f04fc7ed5c136aa419232a5beb466f21508e
Alvaro Parada <alvaro.parada92@gmail.com>
2025-01-20 19:37:38 +0100

    Test the CartContext

---
+++ src/contexts/CartContext/CartContext.test.jsx
@@ -0,0 +1,60 @@
+import { act, fireEvent, render, screen } from "@testing-library/react";
+import { describe, it, expect, vi, beforeEach } from "vitest";
+import { CartProvider } from "./CartContext";
+import {
+  getCartFromLocalStorage,
+  saveCartToLocalStorage,
+  removePhoneFromLocalStorage,
+} from "../../services/cartServices/cartServices";
+import MockUseOfCartContext from "../../mocks/cart/MockUseOfCartContext";
+
+vi.mock("../../services/cartServices/cartServices");
+
+describe("CartContext", () => {
+  beforeEach(() => {
+    getCartFromLocalStorage.mockReturnValue([]);
+  });
+
+  it("should provide an empty cart initially", () => {
+    render(
+      <CartProvider>
+        <MockUseOfCartContext />
+      </CartProvider>
+    );
+
+    expect(screen.getByText("0")).toBeInTheDocument();
+  });
+
+  it("should add a phone to the cart", () => {
+    const phone = { id: "1", name: "Phone 1" };
+
+    render(
+      <CartProvider>
+        <MockUseOfCartContext phone={phone} />
+      </CartProvider>
+    );
+    act(() => {
+      screen.getByText("Add Phone").click();
+    });
+    expect(screen.getByText("1")).toBeInTheDocument();
+    expect(saveCartToLocalStorage).toHaveBeenCalledWith([phone]);
+  });
+
+  it("should remove a phone from the cart", () => {
+    const phone = { id: "1", name: "Phone 1" };
+    getCartFromLocalStorage.mockReturnValue([phone]);
+
+    render(
+      <CartProvider>
+        <MockUseOfCartContext phone={phone} />
+      </CartProvider>
+    );
+
+    act(() => {
+      fireEvent.click(screen.getByText("Remove Phone"));
+    });
+
+    expect(screen.getByText("0")).toBeInTheDocument();
+    expect(removePhoneFromLocalStorage).toHaveBeenCalledWith(phone.id);
+  });
+});
