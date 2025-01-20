4dcbe0c86d041e951b27a02234de1ab3c41debfd
Alvaro Parada <alvaro.parada92@gmail.com>
2025-01-20 19:38:47 +0100

    Implement CartContext

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./components/App/App.jsx";
 import { PhoneProvider } from "./contexts/PhoneContext/PhoneContext";
+import { CartProvider } from "./contexts/CartContext/CartContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <PhoneProvider>
-        <App />
+        <CartProvider>
+          <App />
+        </CartProvider>
       </PhoneProvider>
     </BrowserRouter>
   </StrictMode>
