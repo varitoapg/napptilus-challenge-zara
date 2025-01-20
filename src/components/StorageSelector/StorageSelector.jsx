4d148f4d11255686cdbeeced53d5f94976dec041
Alvaro Parada <alvaro.parada92@gmail.com>
2025-01-20 13:07:36 +0100

    Create and style a selector for capacity

---
+++ src/components/StorageSelector/StorageSelector.jsx
@@ -0,0 +1,53 @@
+import Button from "../Button/Button";
+import PropTypes from "prop-types";
+import "./StorageSelector.css";
+
+function StorageSelector({
+  storageOptions,
+  handleStorageChange,
+  selectedStorage,
+}) {
+  return (
+    <div className="storage-selector">
+      <label htmlFor="storage-options" className="storage-selector__label">
+        Storage, how much space do you need?
+      </label>
+      <div className="storage-selector__buttons" id="storage-options">
+        {storageOptions.map((option) => (
+          <Button
+            key={option.capacity}
+            onClick={() => handleStorageChange(option)}
+            theme="white"
+            className={`storage-selector__button ${
+              selectedStorage?.capacity === option.capacity
+                ? "storage-selector__button--selected"
+                : "storage-selector__button--no-selected"
+            }`}
+            aria-selected={
+              selectedStorage?.capacity === option.capacity ? "true" : "false"
+            }
+            role="button"
+          >
+            {option.capacity}
+          </Button>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+StorageSelector.propTypes = {
+  storageOptions: PropTypes.arrayOf(
+    PropTypes.shape({
+      capacity: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+  handleStorageChange: PropTypes.func.isRequired,
+  selectedStorage: PropTypes.shape({
+    capacity: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+  }),
+};
+
+export default StorageSelector;
