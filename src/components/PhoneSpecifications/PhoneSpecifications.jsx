13448440d5a417a28f42aee629075f49a06929da
Alvaro Parada <alvaro.parada92@gmail.com>
2025-01-20 11:41:06 +0100

    Create a list of phone specifications and style it

---
+++ src/components/PhoneSpecifications/PhoneSpecifications.jsx
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+import { addSpaceBeforeUppercase } from "../../utils/strings/strings";
+import "./PhoneSpecifications.css";
+
+function PhoneSpecifications({ phoneSpecifications }) {
+  return (
+    <div className="phone-specifications">
+      <h2 className="phone-specfications__title">specifications</h2>
+      <ul className="phone-specifications__list">
+        {Object.entries(phoneSpecifications).map(([key, value]) => (
+          <li key={key} className="phone-specifications__item">
+            <h3 className="phone-specifications__title">
+              {addSpaceBeforeUppercase(key)}
+            </h3>
+            <span className="phone-specifications__description">{value}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+PhoneSpecifications.propTypes = {
+  phoneSpecifications: PropTypes.object.isRequired,
+};
+
+export default PhoneSpecifications;
