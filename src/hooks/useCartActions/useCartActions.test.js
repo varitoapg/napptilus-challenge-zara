b8eb22fe8a8d0342fe12c9e4a4f26087a318ce61
Alvaro Parada <alvaro.parada92@gmail.com>
2025-01-21 14:34:08 +0100

    Add handleContinueShopping to useCartActions

--- src/hooks/useCartActions/useCartActions.test.js
+++ src/hooks/useCartActions/useCartActions.test.js
@@ -1,15 +1,23 @@
 import { renderHook, act } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import { useCartActions } from "./useCartActions";
 import { useCartContext } from "../../contexts/CartContext/CartContext";
 import { vi, describe, beforeEach, afterEach, it, expect } from "vitest";
 
+const mockedUseNavigate = vi.fn();
 vi.mock("../../contexts/CartContext/CartContext");
+vi.mock("react-router-dom", () => ({
+  useNavigate: () => mockedUseNavigate,
+  MemoryRouter: ({ children }) => children,
+}));
 
 describe("useCartActions", () => {
   const addPhoneToCart = vi.fn();
   const removePhoneFromCart = vi.fn();
 
   beforeEach(() => {
+    vi.clearAllMocks();
+
     useCartContext.mockReturnValue({
       addPhoneToCart,
       removePhoneFromCart,
@@ -21,7 +29,9 @@ describe("useCartActions", () => {
   });
 
   it("should add phone to cart", () => {
-    const { result } = renderHook(() => useCartActions());
+    const { result } = renderHook(() => useCartActions(), {
+      wrapper: MemoryRouter,
+    });
 
     act(() => {
       result.current.saveToCart({ id: 1, name: "Phone 1" });
@@ -31,7 +41,9 @@ describe("useCartActions", () => {
   });
 
   it("should remove phone from cart", () => {
-    const { result } = renderHook(() => useCartActions());
+    const { result } = renderHook(() => useCartActions(), {
+      wrapper: MemoryRouter,
+    });
 
     act(() => {
       result.current.removeFromCart(1);
