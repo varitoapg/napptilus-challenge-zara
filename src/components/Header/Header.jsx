8daed6363f3864eabcc84aeaeb90f4f84a462d13
Alvaro Parada <alvaro.parada92@gmail.com>
2025-01-22 11:19:00 +0100

    Add navigation back into the Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,32 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import EmptyCartIcon from "../EmptyCartIcon/EmptyCartIcon";
 import FullCartIcon from "../FullCartIcon/FullCartIcon";
 import { useCartInformation } from "../../hooks/useCartInformation/useCartInformation";
 import Logo from "../Logo/Logo";
 import "./Header.css";
+import LeftChevronIcon from "../LeftChevronIcon/LeftChevronIcon";
 
 function Header() {
+  const { pathname } = useLocation();
   const { isCartVisible, phonesInCart } = useCartInformation();
 
   return (
     <header className="header">
-      <Link to="/">
-        <Logo className="header__logo" />
-      </Link>
-      {isCartVisible && (
-        <Link to="/cart" className="header__cart-link">
-          {phonesInCart > 0 ? <FullCartIcon /> : <EmptyCartIcon />}
-          <p>{phonesInCart}</p>
+      <div className="header__content">
+        <Link to="/">
+          <Logo className="header__logo" />
+        </Link>
+        {isCartVisible && (
+          <Link to="/cart" className="header__cart-link">
+            {phonesInCart > 0 ? <FullCartIcon /> : <EmptyCartIcon />}
+            <p>{phonesInCart}</p>
+          </Link>
+        )}
+      </div>
+      {pathname.includes("/phone") && (
+        <Link to="/" className="header__back-link">
+          <LeftChevronIcon />
+          <p>back</p>
         </Link>
       )}
     </header>
