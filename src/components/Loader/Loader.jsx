cb90e453e587a0e4bcce78907be76f6adb21fa29
Alvaro Parada <alvaro.parada92@gmail.com>
2025-01-22 13:52:00 +0100

    Fix a return bug in Loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,7 +1,7 @@
 import "./Loader.css";
 
 function Loader() {
-  <div className="loader" data-testid="loader"></div>;
+  return <div className="loader" data-testid="loader"></div>;
 }
 
 export default Loader;
