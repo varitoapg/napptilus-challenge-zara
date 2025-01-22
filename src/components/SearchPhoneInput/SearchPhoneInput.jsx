4df5275791b4ea0dde71fe77438266a41b624ae2
Alvaro Parada <alvaro.parada92@gmail.com>
2025-01-22 13:10:45 +0100

    Add a hidden label and clear button to SearchPhoneInput

--- src/components/SearchPhoneInput/SearchPhoneInput.jsx
+++ src/components/SearchPhoneInput/SearchPhoneInput.jsx
@@ -1,21 +1,33 @@
 import PropTypes from "prop-types";
 import "./SearchPhoneInput.css";
 
-function SearchPhoneInput({ searchQuery, onSearchChange }) {
+function SearchPhoneInput({ searchQuery, onSearchChange, handleClear }) {
   return (
-    <input
-      type="text"
-      placeholder="Search for a smartphone..."
-      value={searchQuery}
-      onChange={(e) => onSearchChange(e.target.value)}
-      className="search-phone-input"
-    />
+    <div className="search-phone-input-wrapper">
+      <label htmlFor="searchPhoneInput" className="visually-hidden">
+        Search for a smartphone
+      </label>
+      <input
+        id="searchPhoneInput"
+        type="text"
+        placeholder="Search for a smartphone..."
+        value={searchQuery}
+        onChange={(e) => onSearchChange(e.target.value)}
+        className="search-phone-input"
+      />
+      {searchQuery && (
+        <button className="clear-input" onClick={handleClear}>
+          ×
+        </button>
+      )}
+    </div>
   );
 }
 
 SearchPhoneInput.propTypes = {
   searchQuery: PropTypes.string.isRequired,
   onSearchChange: PropTypes.func.isRequired,
+  handleClear: PropTypes.func.isRequired,
 };
 
 export default SearchPhoneInput;
