543d9a7bd851b4cda396820d3c7c4a6a91aa7169
Alvaro Parada <alvaro.parada92@gmail.com>
2025-01-22 20:58:26 +0100

    Update PaymentSection tests

--- src/mocks/hooks/hooks.js
+++ src/mocks/hooks/hooks.js
@@ -9,16 +9,22 @@ export const mockUseCartInformationEmpty = {
   isCartVisible: false,
   havePurchases: false,
   phonesInCart: 0,
+  totalPrice: 0,
+  cart: [],
 };
 
 export const mockUseCartInformationVisible = {
   isCartVisible: true,
   havePurchases: false,
   phonesInCart: 0,
+  totalPrice: 0,
+  cart: [],
 };
 
 export const mockUseCartInformationWithPurchases = {
   isCartVisible: true,
   havePurchases: true,
   phonesInCart: 3,
+  totalPrice: 300,
+  cart: mockedPhoneList,
 };
